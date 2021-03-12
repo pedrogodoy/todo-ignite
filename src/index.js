82b21425d616bb4d06867a0423e8c846614bbcaa
pedrogodoy <pedrogh.godoy12@gmail.com>
2021-03-11 22:17:16 -0400

    Create todos and users route

--- src/index.js
+++ src/index.js
@@ -1,25 +1,48 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(u => u.username === username);
+
+  if(!user) {
+    return response.status(404).json({ error: "User does not exist!" });
+  }
+
+  request.username = username;
+  next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+  
+  const user = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  };
+  
+  users.push(user);
+
+  return response.status(201).send();
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { username } = request;
+
+  const user = users.find(user => user.username === username);
+  return response.status(200).json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
